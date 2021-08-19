86bf154e3c9a77fd78baca5e6f5bbad37b33fd75
HSH2000US <57731842+HSH2000US@users.noreply.github.com>
2021-08-19 02:34:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Press ⌃R to execute it or replace it with your code.binary = str (input ("Enter a binary number "))
+binary = binary [::-1]
+listBin = list(binary.split())
+while len(listBin) % 4 != 0:
+    listBin.append('0')
 
-import pygame
+def listToString(bin):
+    str1 = ""
+    for i in bin:
+        str1 += i
+    return str1
+binary = listToString(listBin)
+
+def convertBin(binary):
+    val = 0
+    for i in range (0, len(binary)):
+        if binary[i] == "1":
+            val += 2 ** i
+    return val
+print(convertBin(binary))
 
 
 
-pygame.init()
 
-
-screen = pygame.display.set_mode((200, 300))
-
-
-while True:
-    screen.fill((255,0,0))
-    pygame.display.flip()
-    pygame.display.update()
 
